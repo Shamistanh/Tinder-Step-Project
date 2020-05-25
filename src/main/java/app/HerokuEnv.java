2a6211d5a966ea8fddfc9e2a19519062cd3b6272
Şəmistan Hüseynov <shamistan1999@gmail.com>
2020-05-25 12:35:43 +0400

    everything works fine - 8

--- src/main/java/app/HerokuEnv.java
+++ src/main/java/app/HerokuEnv.java
@@ -9,22 +9,5 @@ public class HerokuEnv {
       return 5000;
     }
   }
-//  public static String jdbc_url() {
-//    String url = System.getenv("postgres://nyataccyehwxrh:d7d0b02ba6051fee490b31b679376390f5121a6fbed7ed67d7ba50c34ed3cece@ec2-34-200-72-77.compute-1.amazonaws.com:5432/d6t2ihomrv0cr3");
-//    if (url == null) throw new IllegalArgumentException("JDBC_DATABASE_URL is empty!!!");
-//    return url;
-//  }
-//
-//  public static String jdbc_username() {
-//    String url = System.getenv("nyataccyehwxrh");
-//    if (url == null) throw new IllegalArgumentException("JDBC_DATABASE_USERNAME is empty!!!");
-//    return url;
-//  }
-//
-//  public static String jdbc_password() {
-//    String url = System.getenv("d7d0b02ba6051fee490b31b679376390f5121a6fbed7ed67d7ba50c34ed3cece");
-//    if (url == null) throw new IllegalArgumentException("JDBC_DATABASE_PASSWORD is empty!!!");
-//    return url;
-//  }
 
 }
--- src/main/java/db/ConnDetails.java
+++
@@ -1,7 +0,0 @@
-//package db;
-//
-//public class ConnDetails {
-//    public static final String url = "postgres://nyataccyehwxrh:d7d0b02ba6051fee490b31b679376390f5121a6fbed7ed67d7ba50c34ed3cece@ec2-34-200-72-77.compute-1.amazonaws.com:5432/d6t2ihomrv0cr3";
-//    public static final String username = "nyataccyehwxrh";
-//    public static final String password = "d7d0b02ba6051fee490b31b679376390f5121a6fbed7ed67d7ba50c34ed3cece";
-//}
