9e6ec6459a4740e11b7096aafe91124197196689
Şəmistan Hüseynov <shamistan1999@gmail.com>
2020-06-05 21:40:32 +0400

    everything works fine - 11   - readme added

--- src/main/java/app/ServerApp.java
+++ src/main/java/app/ServerApp.java
@@ -31,7 +31,7 @@ public class ServerApp {
 
         handler.addServlet(new ServletHolder(new LikeServlet(engine,conn)), "/liked");
         handler.addServlet(new ServletHolder(new UserServlet(engine,conn)), "/user-list");
-        handler.addServlet(new ServletHolder(new RegisterServlet(conn)), "/");
+       // handler.addServlet(new ServletHolder(new RegisterServlet(conn)), "/");
         handler.addServlet(new ServletHolder(new MessageServlet(engine,conn)), "/messages");
         handler.addServlet(new ServletHolder(new RegisterServlet(conn)), "/register/*");
         handler.addServlet(new ServletHolder(new LoginServlet(conn)), "/login");
